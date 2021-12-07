891776e0e90773b64bc0a836792e06f146b160ab
prasad1555 <shivaprasad.aluri@gmail.com>
2021-12-07 09:45:49 +0530

    JS Intor and Data Types

---
+++ js/fundamentals.js
@@ -0,0 +1,37 @@
+// var - variables
+// 1-
+
+// $_ chars,
+var num = 20;
+
+console.log(num);
+console.log(num + 10);
+console.log(num + 20);
+
+// Data Types
+
+// Primitive:
+// Strings, numbers, bolleans, null, undiefined,
+// [functions, Objects, array]
+
+(" 87987987 ");
+(" true ");
+123123;
+123.32423;
+0.1212;
+0.00001;
+true;
+false;
+null;
+undefined;
+
+// var firstName = "Some x";
+// var lastName = undefined;
+// lastName = "Some y";
+// var isTrue = true;
+// console.log(lastName);
+
+var firstName, lastName, age, isMinor;
+
+firstName = "Siva";
+console.log(firstName);
