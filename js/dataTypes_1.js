adca1c85b8776707241827b167a52e5cec2094c1
prasad1555 <shivaprasad.aluri@gmail.com>
2021-12-09 09:38:26 +0530

    Operators, Arra

---
+++ js/dataTypes_1.js
@@ -0,0 +1,22 @@
+// object, functions, array.
+
+var color1 = "";
+var color2 = "";
+var color3 = "";
+// 0         1        2
+var favColors = ["green", "red", "crimson"];
+
+console.log(favColors);
+console.log(favColors[0]);
+var JS = 123;
+
+// 0   1
+// 0             1         2
+// 0  1  2     3
+// 0      1
+var arr = [JS, ["DataTypes", "Strings", [0, 1, 2, [true, false]]]];
+
+// false;
+
+console.log(arr);
+console.log(arr[1][2][3][1]);
---
+++ js/operators.js
@@ -0,0 +1,28 @@
+// + , -, \, *
+
+var fname = "John";
+var lname = "Doe";
+
+// joining two strings are called concatination
+console.log(fname + lname);
+
+var num = 10;
+num++; // increment
+console.log(num);
+
+num += 10;
+num = num + 10;
+
+num -= 5;
+
+console.log(num);
+
+fname += " Morris";
+console.log(fname);
+
+console.log(1 === "1");
+console.log(1 == "1");
+
+// typeof
+var strType = typeof "asdasdas  ";
+console.log();
